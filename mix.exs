207d60b9cfd08347b060cc197d244edbff553823
Mike Thomas <mthomas@findmypast.com>
2016-07-14 17:24:09 +0100

    Latest updates!

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Vaultex.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison],
+    [applications:
      mod: {Vaultex, []}]
   end
 
