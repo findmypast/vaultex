4d1b3f7779936d5f92d6abd7a9b2a1227be3aae6
Mike Thomas <mthomas@findmypast.com>
2016-07-15 09:16:50 +0100

    Revert accidential changes!

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Vaultex.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications:
+    [applications: [:logger, :httpoison], 
      mod: {Vaultex, []}]
   end
 
