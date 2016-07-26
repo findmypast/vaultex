acd5c30a6475b53984e34c1dc1f342e72bd6e898
Jae Bach Hardie <jhardie@findmypast.com>
2016-07-26 10:26:16 +0100

    Bumped version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Vaultex.Mixfile do
 
   def project do
     [app: :vaultex,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
