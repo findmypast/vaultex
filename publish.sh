2e0e158bb65164b4c8373ca77d53ff95728fb810
Donald Robertson <drobertson@findmypast.com>
2016-10-13 16:37:24 +0100

    Fixed publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mkdir -p /root/.hex && echo -e "$HEX_USER\n$HEX_API_KEY" > /root/.hex/hex.config && echo "$HEX_PASSWORD\ny" | mix hex.publish
+mkdir -p /root/.hex && echo "$HEX_USER\n$HEX_API_KEY" > /root/.hex/hex.config && echo "$HEX_PASSWORD\ny" | mix hex.publish
