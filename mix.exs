68812f22ef15ae69006e0612949af52944bcc3cd
Patrick McDonnell <pmcdonnell@findmypast.com>
2017-01-09 16:04:09 +0000

    Moving with the times and updating httpoison version.

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Vaultex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.10.0"},
+    [{:httpoison, "~> 0.11.0"},
     {:poison, "~> 1.5 or ~> 2.0"},
     {:ex_doc, ">= 0.0.0", only: :dev}]
   end
