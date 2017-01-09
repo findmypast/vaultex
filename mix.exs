382aa992f461548a8412a6a34a227a1431305a68
Patrick McDonnell <pmcdonnell@findmypast.com>
2017-01-09 16:05:54 +0000

    Bumped version number (patch).

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Vaultex.Mixfile do
 
   def project do
     [app: :vaultex,
-     version: "0.2.6",
+     version: "0.2.7",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
