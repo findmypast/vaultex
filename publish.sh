59c82a3664ed27b89ce16d82262adf308dd42b52
Mike Thomas <mthomas@findmypast.com>
2018-01-22 09:23:12 +0000

    SPA1201: Fix publish to hex
    Bloody hex is now asking for a local password as well. Grrrr...
    
    SPA1201: Fix publish to hex
    
    Bloody hex is now asking for a local password as well. Grrrr...

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo "$HEX_USER\n$HEX_PASSWORD" | mix hex.user auth
-echo "y\n$HEX_PASSWORD" | mix hex.publish
+echo "$HEX_USER\n$HEX_PASSWORD\n$LOCAL_PWD\n$LOCAL_PWD\n" | mix hex.user auth
+
+echo "y\n$HEX_PASSWORD\n$LOCAL_PWD" | mix hex.publish
