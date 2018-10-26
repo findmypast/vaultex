fb8d4a85ed372fd8acc11a973155bdf23fa43863
Racter <paracetamolboy@gmail.com>
2018-10-26 15:38:32 +0200

    Added delete feature for the Vaultex client, with documentation and tests.

--- lib/test_doubles/mock_httpoison.ex
+++ lib/test_doubles/mock_httpoison.ex
@@ -31,6 +31,22 @@ defmodule Vaultex.Test.TestDoubles.MockHTTPoison do
     end
   end
 
+  def request(:delete, url, _params, [{"X-Vault-Token", token}, {"Content-Type", "application/json"}], _opts) do
+    vault_response(token, "whatever", url)
+  end
+
+  def request(:delete, url, _params, _, _) do
+    cond do
+      url |> String.contains?("secret/foo") -> {:ok, %{status_code: 204,
+                                                      headers: [{"Location", redirect_url(url)}],
+                                                      body: Poison.Encoder.encode(%{},[])}}
+      url |> String.contains?("secret/baz") -> {:ok, %{status_code: 204, body: Poison.Encoder.encode(%{"errors" => []},[])}}
+      url |> String.contains?("secret/faz") -> {:ok, %{status_code: 204, body: Poison.Encoder.encode(%{"errors" => ["Not Authenticated"]},[])}}
+      url |> String.contains?("secret/boom") -> {:error, %HTTPoison.Error{id: nil, reason: :econnrefused}}
+      :else -> {:ok, %{} }
+    end
+  end
+
   defp vault_response(key, status_code, url) do
     cond do
       key |> String.contains?("good") -> {:ok, %{status_code: status_code,
