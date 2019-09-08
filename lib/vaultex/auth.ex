d9ae10667656bddfcce7f011f9f89b87d4ce02e1
Steffan Levet <steffan@narrative.so>
2019-09-09 07:51:03 +1200

    added aws iam instance role method

--- lib/vaultex/auth.ex
+++ lib/vaultex/auth.ex
@@ -11,6 +11,10 @@ defmodule Vaultex.Auth do
     handle(:aws, Vaultex.Auth.AWSIAM.credentials(role, server), state)
   end
 
+  def handle(:aws_instance, {role, server}, state) do
+    handle(:aws, Vaultex.Auth.AWSInstanceRole.credentials(role, server), state)
+  end
+
   def handle(:userpass, {username, password}, state) do
     handle(:userpass, %{username: username, password: password}, state)
   end
---
+++ lib/vaultex/aws_instancerole.ex
@@ -0,0 +1,17 @@
+defmodule Vaultex.Auth.AWSInstanceRole do
+  @moduledoc """
+  Helper functions for AWS IAM instance metadata role authentication
+  """
+
+  @url "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7/"
+
+  def credentials(role, server) do
+
+    {:ok, %{body: body}} = HTTPoison.post(@url, "")
+    %{
+      role: role,
+      pkcs7: body
+    }
+  end
+
+end
