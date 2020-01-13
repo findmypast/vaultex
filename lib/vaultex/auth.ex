c5cdfd64d40057718673849ebb1335a3bfb21c9f
Steffan Levet <steffan@narrative.so>
2020-01-14 09:17:40 +1300

    change to set path to nonce regardless of binary call site

--- lib/vaultex/auth.ex
+++ lib/vaultex/auth.ex
@@ -11,26 +11,32 @@ defmodule Vaultex.Auth do
     handle(:aws, Vaultex.Auth.AWSIAM.credentials(role, server), state)
   end
 
-  def handle(:aws_instance, {role, _server}, %{nonce: nonce} = state) do
+  def handle(:aws_instance, {role, _nonce_path}, %{nonce: nonce} = state) do
     IO.inspect("ABOUT TO DO AUTH")
     handle(:aws, Vaultex.Auth.AWSInstanceRole.credentials(role, nonce), state)
   end
 
-  def handle(:aws_instance, {role, _server}, state) do
-    nonce = 
-      case File.read(".nonce") do
+  def handle(:aws_instance, {role, nonce_path}, state) do
+    nonce =
+      case File.read("#{nonce_path}.nonce") do
         {:ok, nonce} ->
           IO.inspect("found from file")
           nonce
+
         {:error, _} ->
           n = UUID.uuid4()
-          File.write(".nonce", n)
+          File.write("#{nonce_path}.nonce", n)
           IO.inspect("making new nonce: #{n}")
           n
       end
-    
+
     IO.inspect("HAVE NONCE: #{nonce}")
-    handle(:aws, Vaultex.Auth.AWSInstanceRole.credentials(role, nonce), Map.put(state, :nonce, nonce))
+
+    handle(
+      :aws,
+      Vaultex.Auth.AWSInstanceRole.credentials(role, nonce),
+      Map.put(state, :nonce, nonce)
+    )
   end
 
   def handle(:userpass, {username, password}, state) do
@@ -55,22 +61,32 @@ defmodule Vaultex.Auth do
 
   # auth method with usernames are expected to call `POST auth/:method/login/:username`
   def handle(method, %{username: username} = credentials, state) do
-    request(:post, "#{state.url}auth/#{method}/login/#{username}", credentials, [{"Content-Type", "application/json"}])
+    request(:post, "#{state.url}auth/#{method}/login/#{username}", credentials, [
+      {"Content-Type", "application/json"}
+    ])
     |> handle_response(state)
   end
 
   # Generic login behavior for most methods
   def handle(method, credentials, state) when is_map(credentials) do
     IO.inspect(credentials)
-    request(:post, "#{state.url}auth/#{method}/login", credentials, [{"Content-Type", "application/json"}])
+
+    request(:post, "#{state.url}auth/#{method}/login", credentials, [
+      {"Content-Type", "application/json"}
+    ])
     |> handle_response(state)
   end
 
   defp handle_response({:ok, response}, state) do
-    case response.body |> Poison.Parser.parse! do
-      %{"errors" => messages} -> {:reply, {:error, messages}, state}
-      %{"auth" => nil, "data" => data} -> {:reply, {:ok, :authenticated}, Map.merge(state, %{token: data["id"]})}
-      %{"auth" => properties} -> {:reply, {:ok, :authenticated}, Map.merge(state, %{token: properties["client_token"]})}
+    case response.body |> Poison.Parser.parse!() do
+      %{"errors" => messages} ->
+        {:reply, {:error, messages}, state}
+
+      %{"auth" => nil, "data" => data} ->
+        {:reply, {:ok, :authenticated}, Map.merge(state, %{token: data["id"]})}
+
+      %{"auth" => properties} ->
+        {:reply, {:ok, :authenticated}, Map.merge(state, %{token: properties["client_token"]})}
     end
   end
 
