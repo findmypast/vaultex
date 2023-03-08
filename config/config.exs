a51df904fd71d879d439722fe0e454c127e4fcac
lauragrechenko <75909645+lauragrechenko@users.noreply.github.com>
2023-03-08 10:03:21 +0100

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -18,7 +18,7 @@ use Mix.Config
 #
 # Or configure a 3rd-party app:
 #
-config :logger, :console, level: :info
+# config :logger, :console, level: :info
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
@@ -28,6 +28,6 @@ config :logger, :console, level: :info
 #
 #     import_config "#{Mix.env}.exs"
 
-# config :vaultex, httpoison: HTTPoison
+config :vaultex, httpoison: HTTPoison
 
 import_config "#{Mix.env()}.exs"
